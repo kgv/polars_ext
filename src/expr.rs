491c65bda270fb70ae4fe7ba5c2f08a02b50066c
kgv <kgv@users.noreply.github.com>
2025-01-27 08:17:08 +0300

    0.0.2

--- src/expr.rs
+++ src/expr.rs
@@ -3,22 +3,14 @@ use polars::prelude::*;
 
 /// Extension methods for [`Expr`]
 pub trait ExprExt {
-    fn clip_min_if(self, clip: bool) -> Expr;
-
     fn destruct(self, names: impl IntoIterator<Item = impl AsRef<str>>) -> Expr;
 
     fn hash(self) -> Expr;
 
     fn normalize(self) -> Expr;
-
-    fn normalize_if(self, normalize: bool) -> Expr;
 }
 
 impl ExprExt for Expr {
-    fn clip_min_if(self, clip: bool) -> Expr {
-        if clip { self.clip_min(lit(0)) } else { self }
-    }
-
     fn destruct(mut self, names: impl IntoIterator<Item = impl AsRef<str>>) -> Expr {
         for name in names {
             self = self.struct_().field_by_name(name.as_ref());
@@ -35,6 +27,19 @@ impl ExprExt for Expr {
     /// Normalize column, type [`f64`], the same name
     fn normalize(self) -> Expr {
         self.apply(column(normalize), GetOutput::same_type())
+    }
+}
+
+/// Extension `if` methods for [`Expr`]
+pub trait ExprIfExt: ExprExt {
+    fn clip_min_if(self, clip: bool) -> Expr;
+
+    fn normalize_if(self, normalize: bool) -> Expr;
+}
+
+impl ExprIfExt for Expr {
+    fn clip_min_if(self, clip: bool) -> Expr {
+        if clip { self.clip_min(lit(0)) } else { self }
     }
 
     fn normalize_if(self, normalize: bool) -> Expr {
