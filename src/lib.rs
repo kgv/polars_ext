c86e4e53e63d79f6619e95242324522a252b9643
kgv <kgv@users.noreply.github.com>
2025-04-28 15:24:57 +0300

    Add temporal_conversions feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 pub mod prelude {
-    pub use crate::{data_frame::DataFrameExt, expr::ExprExt};
+    pub use crate::{
+        data_frame::DataFrameExt,
+        expr::{ExprExt, ExprIfExt},
+    };
+    #[cfg(feature = "temporal_conversions")]
+    pub use polars_arrow::temporal_conversions::{
+        timestamp_ms_to_datetime, timestamp_ns_to_datetime, timestamp_us_to_datetime,
+    };
 }
 
 pub mod column;
