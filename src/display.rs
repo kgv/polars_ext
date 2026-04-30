cdf78792cde5b2f29daa3d03f6330f6cfc67cd9f
kgv <kgv@users.noreply.github.com>
2026-04-30 12:01:46 +0300

    Fix option

--- src/display.rs
+++ src/display.rs
@@ -1,8 +1,8 @@
 use crate::r#const::EM_DASH;
 use std::fmt::{Display, from_fn};
 
-pub fn option<T: Display>(option: &Option<T>) -> impl Display {
-    from_fn(move |f| match option {
+pub fn option<T: Display>(option: Option<T>) -> impl Display {
+    from_fn(move |f| match &option {
         None => f.write_str(EM_DASH),
         Some(t) => Display::fmt(t, f),
     })
