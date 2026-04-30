442ee4356a989690eafb90ec02335d82314ea013
kgv <kgv@users.noreply.github.com>
2026-04-30 12:22:55 +0300

    Add DisplayOption

--- src/display.rs
+++ src/display.rs
@@ -1,9 +1,23 @@
 use crate::r#const::EM_DASH;
 use std::fmt::{Display, from_fn};
 
-pub fn option<T: Display>(option: Option<T>) -> impl Display {
-    from_fn(move |f| match &option {
-        None => f.write_str(EM_DASH),
-        Some(t) => Display::fmt(t, f),
-    })
+/// Display option
+pub trait DisplayOption {
+    fn display(self) -> impl Display;
 }
+
+impl<T: Display> DisplayOption for Option<T> {
+    fn display(self) -> impl Display {
+        from_fn(move |f| match &self {
+            None => f.write_str(EM_DASH),
+            Some(t) => Display::fmt(t, f),
+        })
+    }
+}
+
+// pub fn option<T: Display>(option: Option<T>) -> impl Display {
+//     from_fn(move |f| match &option {
+//         None => f.write_str(EM_DASH),
+//         Some(t) => Display::fmt(t, f),
+//     })
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod prelude {
     #[cfg(feature = "array")]
     pub use crate::expr::eval_arr;
-    pub use crate::{data_frame::DataFrameExt, display::option, expr::ExprExt};
+    pub use crate::{data_frame::DataFrameExt, display::DisplayOption, expr::ExprExt};
     #[cfg(feature = "temporal_conversions")]
     pub use polars_arrow::temporal_conversions::{
         timestamp_ms_to_datetime, timestamp_ns_to_datetime, timestamp_us_to_datetime,
