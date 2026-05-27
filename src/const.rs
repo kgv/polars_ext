514adb6aab73b8a7baf982c8959ebc777614a381
kgv <kgv@users.noreply.github.com>
2026-05-27 18:14:37 +0300

    112

--- src/const.rs
+++ src/const.rs
@@ -3,4 +3,5 @@ pub const NO_BREAK_SPACE: &str = " ";
 
 pub const ARRAY: &str = "Array";
 pub const MEAN: &str = "Mean";
+pub const RELATIVE_STANDARD_DEVIATION: &str = "RelativeStandardDeviation";
 pub const STANDARD_DEVIATION: &str = "StandardDeviation";
--- src/expr/array.rs
+++ src/expr/array.rs
@@ -1,5 +1,5 @@
 use crate::{
-    r#const::{ARRAY, MEAN, STANDARD_DEVIATION},
+    r#const::{ARRAY, MEAN, RELATIVE_STANDARD_DEVIATION, STANDARD_DEVIATION},
     expr::ExprExt,
 };
 use const_format::formatcp;
@@ -27,38 +27,84 @@ pub struct Array {
     significant: bool,
 
     #[builder(default)]
+    flatten: bool,
+    #[builder(default)]
     percent: bool,
 }
 
 impl From<Array> for Expr {
     fn from(value: Array) -> Self {
-        as_struct(vec![
-            value.expr.clone().arr().eval(
-                element()
-                    .percent(value.percent)
-                    .precision(value.precision, value.significant),
-                false,
-            ),
-            value
-                .expr
-                .clone()
+        let mut array = value.expr.clone().arr().eval(
+            element()
+                .percent(value.percent)
+                .precision(value.precision, value.significant),
+            false,
+        );
+        if value.flatten {
+            array = array
                 .arr()
-                .mean()
+                .to_struct(Some(PlanCallback::new(move |index| {
+                    Ok(format!("{ARRAY}[{index}]"))
+                })))
+                .struct_()
+                .field_by_name("*");
+        } else {
+            array = array.alias(ARRAY);
+        }
+        let mean = value.expr.clone().arr().mean();
+        let standard_deviation = value.expr.clone().arr().std(value.ddof);
+        let mut expr = as_struct(vec![
+            array,
+            mean.clone()
                 .percent(value.percent)
                 .precision(value.precision, value.significant)
-                .name()
-                .suffix(MEAN),
-            value
-                .expr
+                .alias(MEAN),
+            standard_deviation
                 .clone()
-                .arr()
-                .std(value.ddof)
                 .percent(value.percent)
                 .precision(value.precision, value.significant)
-                .name()
-                .suffix(STANDARD_DEVIATION),
+                .alias(STANDARD_DEVIATION),
+            (standard_deviation / mean)
+                .percent(true)
+                .precision(value.precision, value.significant)
+                .alias(RELATIVE_STANDARD_DEVIATION),
         ])
-        .struct_()
-        .rename_fields(vec![ARRAY, MEAN, STANDARD_DEVIATION])
+        .name()
+        .keep();
+        if value.flatten {
+            expr = expr.struct_().field_by_name("*");
+        }
+        expr
     }
 }
+
+// #[test]
+// fn test() {
+//     use polars::prelude::*;
+
+//     let mut df = df! {
+//         "id" => &[1, 2, 3],
+//         "my_arrays" => &[
+//             Series::new("".into(), &[1.0, 2.0, 3.0]),
+//             Series::new("".into(), &[4.0, 5.0, 6.0]),
+//             Series::new("".into(), &[7.0, 8.0, 9.0]),
+//         ]
+//     }
+//     .unwrap();
+//     df.try_apply("my_arrays", |series| {
+//         series.cast(&DataType::Array(Box::new(DataType::Float64), 3))
+//     })
+//     .unwrap();
+//     let lf = df.lazy().with_column(
+//         Array::builder()
+//             .expr(col("my_arrays"))
+//             .ddof(1)
+//             .percent(true)
+//             .precision(2)
+//             .significant(false)
+//             .flatten(false)
+//             .build().struct_().field_by_name("*"),
+//     );
+//     println!(": {}", lf.clone().collect().unwrap());
+//     // df.lazy().
+// }
